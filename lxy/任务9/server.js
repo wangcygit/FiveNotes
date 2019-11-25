d3885b19d327d3fd15f220b6e22de8643d9ea3d5
liuxinyao5 <348828847@qq.com>
2019-11-25 16:26:31 +0800

    小结

---
+++ lxy/任务9/server.js
@@ -0,0 +1,24 @@
+var net = require('net');
+var clientList = [];
+var server = net.createServer(function (socket) {
+	clientList.push(socket);
+	socket.write('success! \r\n');
+	//socket.pipe(socket);
+	socket.on('data', function(data){
+		console.log(data.toString());
+		//socket.write(data);
+		broadcast(data);
+	});
+	socket.on('end', function(){
+		//socket.write('end!');
+		clientList = [];
+	});
+});
+
+function broadcast(data){
+	for(var i=0; i<clientList.length; i++){
+		clientList[i].write(data);
+	}
+}
+
+server.listen(1337, '127.0.0.1');
